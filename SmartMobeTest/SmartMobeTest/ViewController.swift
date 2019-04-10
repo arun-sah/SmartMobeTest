709b7abe09b93dc1f3c3f1cfed54c54b0336b819
Arun Kumar Sah <arun.shah2012@gmail.com>
2019-04-10 22:17:45 +0545

    Project Setup

---
+++ SmartMobeTest/SmartMobeTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartMobeTest
+//
+//  Created by Arun kumar Sah on 4/10/19.
+//  Copyright © 2019 Arun kumar Sah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
